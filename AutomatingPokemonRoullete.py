275ac6c45b254f3c63bf9af0337440a403cb80fb
boechat <15213557+boechat@users.noreply.github.com>
2025-09-08 15:51:33 -0300

    Update AutomatingPokemonRoullete.py

--- AutomatingPokemonRoullete.py
+++ AutomatingPokemonRoullete.py
@@ -1,5 +1,4 @@
-##### 
-'''
+######################################################################################################################'''
 Projeto para replicar a automação criada em Automation Anywhere, para jogar o "POKEMON ROULETTE"
 
 https://zeroxm.github.io/pokemon-roulette/
